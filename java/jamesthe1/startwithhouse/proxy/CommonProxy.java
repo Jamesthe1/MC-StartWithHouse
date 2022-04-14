0ae472c5a7caf50fd2dfa01f05499570a2ba330f
James <jamesthe1sky@gmail.com>
2022-04-14 16:13:03 -0500

    Delete java/jamesthe1/startwithhouse directory

--- java/jamesthe1/startwithhouse/proxy/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package jamesthe1.startwithhouse.proxy;
-
-import net.minecraft.item.Item;
-
-public class CommonProxy {
-	public void registerItemRenderer (Item item, int meta, String id) {
-		
-	}
-}
